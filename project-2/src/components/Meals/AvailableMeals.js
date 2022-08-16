49ad5c930fc4c7fbe8707892942b701e4ec8afd6
500PING <ndt512x@gmail.com>
2022-08-16 17:28:35 +0700

    151

--- project-2/src/components/Meals/AvailableMeals.js
+++ project-2/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -31,12 +33,22 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = (props) => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
